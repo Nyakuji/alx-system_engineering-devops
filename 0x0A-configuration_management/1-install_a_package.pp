5c05e3516a57076cdf2bc39ec59f77e86536d77d
Philip Opiyo <opiyophilipo@gmail.com>
2024-01-19 12:48:45 -0800

    2-execute_a_command.pp

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -3,10 +3,3 @@ package { 'flask':
     ensure   => '2.1.0',
     provider => 'pip3',
 }
-#install Werkzeug 2.1.1
-package { 'Werkzeug':
-    ensure   => '2.1.1',
-    provider => 'pip3'
-    require => 
-    package['Flask'],
-}
